f6774ef8d37012b71c20f6d6eda16a5aa7d76ff1
Mark Hesketh <contact@markhesketh.co.uk>
2013-05-20 22:59:20 +0100

    Adding files.

---
+++ assets/simplesyntax.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function( $ ) {
+	// Add to pre tags
+	$('pre').addClass('prettyprint');
+
+	// Start the magic
+	prettyPrint();
+});
---
+++ simplesyntax.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name: Simple Syntax
+Description: No hassle way to highlight your <code>&lt;pre&gt;</code> and <code>&lt;code&gt;</code> tags using Prettify.js
+Version: 1.0
+License: GPL v3
+Author: Mark Hesketh
+Author URI: http://www.markhesketh.co.uk
+*/
+
+/*
+* Load prettify.js
+*/
+function ss_load_js() {
+    wp_deregister_script( 'prettify' );
+    wp_register_script( 'prettify', WP_PLUGIN_URL . '/' . basename(dirname(__FILE__)).  '/assets/prettify.js', NULL, false, true);
+    wp_enqueue_script( 'prettify' );
+}
+add_action('wp_enqueue_scripts', 'ss_load_js');
+
+/*
+* Load simplesyntax.js
+*/
+function ss_load_simplesyntax() {
+    wp_deregister_script( 'simplesyntax' );
+    wp_register_script( 'simplesyntax', WP_PLUGIN_URL . '/' . basename(dirname(__FILE__)) . '/assets/simplesyntax.js', array('jquery'), false, true);
+    wp_enqueue_script( 'simplesyntax' );
+}
+add_action('wp_enqueue_scripts', 'ss_load_simplesyntax');
+
+/*
+* Load prettify.css
+*/
+function ss_load_css() {
+	wp_deregister_style( 'prettify' );
+	wp_register_style('prettify', WP_PLUGIN_URL . '/' . basename(dirname(__FILE__)) . '/assets/prettify2.css');
+	wp_enqueue_style('prettify');
+}
+add_action('wp_enqueue_scripts', 'ss_load_css');
+
+/**
+ * Add 'prettyprint' class to <code> and <pre> tags
+ */
+function ss_add_prettyprint_class( $content ) {
+
+    // Add <code> tags within the <pre>
+    $content = str_replace('<pre>', '<pre><code>', $content);
+    $content = str_replace('</pre>', '</code></pre>', $content);
+
+    $content = str_replace('<code><code>', '<code>', $content);
+    $content = str_replace('</code></code>', '</code>', $content);
+
+    return $content;
+}
+add_filter( 'the_content', 'ss_add_prettyprint_class' );
